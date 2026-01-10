8476f0d90c3eda1ebfa666135c3c486f337275bf
zsecgo-senior <zsec.go@gmail.com>
2026-01-10 17:34:39 +0500

    new changhes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<div className='w-screen h-screen bg-linear-to-r from-[#1d43d8] via-[#DC2E56] to-[#3451C3]'>
+			<App />
+		</div>
+	</StrictMode>
 )
