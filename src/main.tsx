ba1798c492172cbaefb60688d19118cd6d6c04e8
zsecgo-senior <zsec.go@gmail.com>
2026-01-10 17:47:23 +0500

    new app

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<div className='w-screen h-screen bg-linear-to-r from-[#1d43d8] via-[#DC2E56] to-[#3451C3]'>
-			<App />
-		</div>
+		<App />
 	</StrictMode>
 )
